63cc699d21c6b3c6e91f7f44df444d39bca98bfc
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2020-12-06 01:40:20 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-const DB = "mongodb://localhost:27017/softxDB";
+const DB = "mongodb+srv://<username>:<password>@cluster0.ck3hh.mongodb.net/<dbname>?retryWrites=true&w=majority";
 module.exports = DB;
