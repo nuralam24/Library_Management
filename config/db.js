ea98f0891b00b53e4dc756895bf2b814ed66a2ed
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2020-12-06 01:41:36 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-const DB = "mongodb+srv://<username>:<password>@cluster0.ck3hh.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const DB = "mongodb+srv://nuralam2020@cluster0.ck3hh.mongodb.net/<dbname>?retryWrites=true&w=majority";
 module.exports = DB;
